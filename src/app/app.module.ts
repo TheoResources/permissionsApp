6722d4a1783c0dba068b03e468db0b4ddc359567
Michal Matlosz <michal.matlosz@allegro.pl>
2019-05-16 19:19:25 +0200

    init app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    NgbModule,
     BrowserModule,
     AppRoutingModule
   ],
