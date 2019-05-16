56ea6800dccec4b836f633a72ba465bef4276d6e
Michal Matlosz <michal.matlosz@allegro.pl>
2019-05-16 19:13:29 +0200

    init app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+
+interface Presentation {
+  id: string;
+  name: string;
+  author: string;
+  description: string;
+}
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <h1>Panel administracyjny</h1>
+    <p>Lista prezentacji</p>
+    <table class="table table-striped">
+      <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Nazwa</th>
+        <th scope="col">Opis</th>
+        <th scope="col">Autor</th>
+        <th scope="col"></th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr *ngFor="let presentation of presentations; index as i">
+        <th scope="row">{{ i + 1 }}</th>
+        <td>
+          {{ presentation.name }}
+        </td>
+        <td>{{ presentation.description  }}</td>
+        <td>{{ presentation.author}}</td>
+        <td><a href="/edit">Edycja</a></td>
+      </tr>
+      </tbody>
+    </table>
+  `,
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'permissions-app';
+  presentations = [
+    {
+      id: 'id1',
+      name: 'CSP, czyli rozbrajanie bomby w praktyce',
+      author: 'Paweł Czubachowski',
+      description: 'Content Security Policy to mechanizm, który ma chronić użytkowników m.in. przed podatnością XSS. Jego poprawne ...'
+    },
+    {
+      id: 'id2',
+      name: 'BigData w oparciu o rozwiązania Google',
+      author: 'Michał Karykowski',
+      description: 'Wszyscy wiedzą, że rozwiązania chmurowe są na topie, a każdy dostawca specjalizuje się w jakiejś gałęzi ...'
+    },
+    {
+      id: 'id3',
+      name: 'Docker w służbie dev(ops)a',
+      author: 'Marcin Jasiński',
+      description: 'Docker w ostatnim czasie stał się bardzo popularny, zarówno wśród gigantow takich jak Google, jak ...'
+    }
+  ] as Presentation[];
+
 }
