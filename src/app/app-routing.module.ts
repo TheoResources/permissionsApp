419176b42949cf1a04d3eee5a51e57631d2be87a
Michal Matlosz <michal.matlosz@allegro.pl>
2019-05-29 18:12:20 +0200

    guard formatting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {ListPresentationsComponent} from './presentations/list-presentations.component';
 import {AuthGuardService} from './auth/auth-guard.service';
 
 const appRoutes: Routes = [
-  {path: 'list', component: ListPresentationsComponent, canActivate: [AuthGuardService], data: {auth: 'ReadOnly'}}
-];
+  {
+    path: 'list',
+    component: ListPresentationsComponent,
+    canActivate: [AuthGuardService],
+    data: {auth: 'ReadOnly'}
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
